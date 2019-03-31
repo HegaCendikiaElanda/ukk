a41fe65a7733e48d73f25e7b04f3c614a4a11b68
Hega Cendikia Elanda <42016968+HegaCendikiaElanda@users.noreply.github.com>
2019-03-31 14:27:03 +0700

    ahh

---
+++ src/listrik_ukk/Listrik_ukk.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listrik_ukk;
+import view.login;
+/**
+ *
+ * @author Adnale
+ */
+public class Listrik_ukk {
+    login Login;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new login().setVisible(true);
+    }
+    
+}
